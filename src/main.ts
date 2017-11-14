f5c870d7df14a26dacc38bfc1814bcd518c3fa88
Teenak <teenak77@gmail.com>
2017-11-14 11:40:48 +0900

    Added express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 
+import * as express from "express";
+
+const app: express.Application = express();
+const port: number = Number(process.env.PORT) || 3000;
+
 export class Startup {
+
 	public static main(): number {
 
-		console.log("hello");
+		app.get('/', function (_, res) {
+			res.send('Hello World!');
+		});
+
+		app.listen(port, function () {
+			console.log(`Example app listening on port ${port}!`);
+		});
 
 		return 0;
 	}
